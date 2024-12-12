94e0659bcad728ea21475a479356be2bd40a0330
Gary Fan <tomfan419@gmail.com>
2024-12-13 03:59:30 +0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Gary on 13/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
