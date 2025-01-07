955a5441bac4d5a1273db0ba3e111d56ad3b4b3b
Gary Fan <tomfan419@gmail.com>
2025-01-08 07:44:57 +0800

    Added split function

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -2,20 +2,72 @@
 //  ContentView.swift
 //  WeSplit
 //
-//  Created by Gary on 13/12/2024.
+//  Created by IOS Dev Ninja on 13/12/2024.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @FocusState private var amountIsFocused: Bool
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalAmount: Double {
+        let tipValue = checkAmount/100 * Double(tipPercentage)
+        return checkAmount + tipValue
+    }
+    
+    var totalPerPerson: Double {
+        let amountPerPerson = totalAmount / Double(numberOfPeople + 2)
+        return amountPerPerson
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amout", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+//                    .pickerStyle(.navigationLink)
+                    
+                }
+                Section("How much tip do you want to leave?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<101) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                Section("Total Amount") {
+                    Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundStyle(tipPercentage == 0 ? .red : .white)
+                }
+                
+                Section("Amount Per Person") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .navigationBarTitleDisplayMode(.automatic)
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
